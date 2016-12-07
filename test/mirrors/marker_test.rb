e0ff0e4de37f0b895b4c4f3e27885f8780df50c4
Burke Libbey <burke@libbey.me>
2016-12-07 14:12:06 -0500

    fix tests

--- test/mirrors/marker_test.rb
+++ test/mirrors/marker_test.rb
@@ -28,7 +28,7 @@ module Mirrors
       assert_equal(Marker::TYPE_CLASS_REFERENCE, @class_marker.type)
       assert_equal('Class', @class_marker.message)
       assert_equal(__FILE__, @class_marker.file)
-      assert_equal(6, @class_marker.line)
+      assert_equal(10, @class_marker.line)
       assert_equal(Marker::NO_COLUMN, @class_marker.start_column)
       assert_equal(Marker::NO_COLUMN, @class_marker.end_column)
     end
@@ -37,7 +37,7 @@ module Mirrors
       assert_equal(Marker::TYPE_FIELD_REFERENCE, @field_marker.type)
       assert_equal('Field', @field_marker.message)
       assert_equal(__FILE__, @field_marker.file)
-      assert_equal(7, @field_marker.line)
+      assert_equal(16, @field_marker.line)
       assert_equal(Marker::NO_COLUMN, @field_marker.start_column)
       assert_equal(Marker::NO_COLUMN, @field_marker.end_column)
     end
@@ -46,7 +46,7 @@ module Mirrors
       assert_equal(Marker::TYPE_METHOD_REFERENCE, @method_marker.type)
       assert_equal('Method', @method_marker.message)
       assert_equal(__FILE__, @method_marker.file)
-      assert_equal(8, @method_marker.line)
+      assert_equal(22, @method_marker.line)
       assert_equal(Marker::NO_COLUMN, @method_marker.start_column)
       assert_equal(Marker::NO_COLUMN, @method_marker.end_column)
     end
