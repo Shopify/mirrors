d3ef06d87cc4b3051d5a510b39e7bd53730c219e
Burke Libbey <burke@libbey.me>
2016-12-08 23:56:01 -0500

    references test

--- test/mirrors/references_visitor_test.rb
+++ test/mirrors/references_visitor_test.rb
@@ -13,21 +13,16 @@ module Mirrors
     end
 
     def test_victim_class
-      method = Mirrors.reflect(Victim).instance_methods.first
-      refs = method.references
-      assert_equal(6, refs.size)
-      c1 = Marker.new(type: Marker::TYPE_CLASS_REFERENCE, message: :Kernel, file: __FILE__, line: 10)
-      m1 = Marker.new(type: Marker::TYPE_METHOD_REFERENCE, message: :to_s, file: __FILE__, line: 9)
-      m2 = Marker.new(type: Marker::TYPE_METHOD_REFERENCE, message: :exit, file: __FILE__, line: 10)
-      m3 = Marker.new(type: Marker::TYPE_METHOD_REFERENCE, message: :foo, file: __FILE__, line: 11)
-      m3 = Marker.new(type: Marker::TYPE_METHOD_REFERENCE, message: :baz, file: __FILE__, line: 11)
-      f1 = Marker.new(type: Marker::TYPE_FIELD_REFERENCE, message: :@ivar, file: __FILE__, line: 8)
-
-      assert_equal(:lol, method.name) # ensure that we have the right method
-      assert(refs.include?(c1))
-      assert(refs.include?(m1))
-      assert(refs.include?(m2))
-      assert(refs.include?(f1))
+      actual = Mirrors.reflect(Victim).instance_method(:lol).references
+      expected = [
+        Marker.new(type: Marker::TYPE_FIELD_REFERENCE, message: :@ivar, file: __FILE__, line: 8),
+        Marker.new(type: Marker::TYPE_METHOD_REFERENCE, message: :to_s, file: __FILE__, line: 9),
+        Marker.new(type: Marker::TYPE_CLASS_REFERENCE, message: :Kernel, file: __FILE__, line: 10),
+        Marker.new(type: Marker::TYPE_METHOD_REFERENCE, message: :exit, file: __FILE__, line: 10),
+        Marker.new(type: Marker::TYPE_METHOD_REFERENCE, message: :foo, file: __FILE__, line: 11),
+        Marker.new(type: Marker::TYPE_METHOD_REFERENCE, message: :baz, file: __FILE__, line: 11),
+      ]
+      assert_equal(expected, actual)
     end
   end
 end
